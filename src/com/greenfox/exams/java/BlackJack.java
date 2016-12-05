75454dcc32bb8593c508792c1cde817235378f9a
ZSOFI PRINCZ <zsofiaprincz@gmail.com>
2016-12-05 10:00:26 +0100

    Panel and items on panel are created and it displays

--- src/com/greenfox/exams/java/BlackJack.java
+++ src/com/greenfox/exams/java/BlackJack.java
@@ -1,7 +1,57 @@
 package com.greenfox.exams.java;
 
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 /**
  * Created by zsofiaprincz on 05/12/16.
  */
-public class BlackJack {
+public class BlackJack extends JFrame implements ActionListener {
+
+    JPanel blackJack = new JPanel();
+    JLabel user = new JLabel("User played");
+    JLabel house = new JLabel("House played");
+    JButton reset = new JButton("New Game");
+    JButton drawCard = new JButton("Draw a drawCard");
+
+    public BlackJack(){
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setResizable(false);
+        this.setLocationRelativeTo(null);
+        this.setVisible(true);
+        this.setSize(400, 400);
+
+        this.add(blackJack);
+        blackJack.add(user);
+        blackJack.add(house);
+        blackJack.add(reset);
+        blackJack.add(drawCard);
+
+
+
+    }
+
+    public static void main(String[] args) {
+
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new BlackJack();
+            }
+        });
+
+    }
+
+
+
+
+
+
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
 }
---
+++ src/com/greenfox/exams/java/Deck.java
@@ -0,0 +1,14 @@
+package com.greenfox.exams.java;
+
+import java.util.ArrayList;
+
+/**
+ * Created by zsofiaprincz on 05/12/16.
+ */
+public class Deck {
+
+    ArrayList<String>cards = new ArrayList<String>();
+    ArrayList<String>used = new ArrayList<String>();
+
+
+}
